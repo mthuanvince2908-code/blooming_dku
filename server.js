8892efc67dccae183233ad6d145fe13e6b09436d
mthuanvince2908-code <mthuanvince2908@gmail.com>
2026-04-24 16:02:35 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
 
-let latestValue = 0;
+let latest = { s1: 0, s2: 0 };
 
 app.get('/update', (req, res) => {
-  latestValue = req.query.value;
+  latest.s1 = req.query.s1;
+  latest.s2 = req.query.s2;
+
+  console.log("Received:", latest);
   res.send("OK");
 });
 
 app.get('/', (req, res) => {
-  res.send(`<h1>Sensor: ${latestValue}</h1>`);
+  res.send(`
+    <h1>Sensor Dashboard</h1>
+    <p>Sensor 1: ${latest.s1}</p >
+    <p>Sensor 2: ${latest.s2}</p >
+    <script>
+      setTimeout(() => location.reload(), 2000);
+    </script>
+  `);
 });
 
 app.listen(process.env.PORT || 3000);
